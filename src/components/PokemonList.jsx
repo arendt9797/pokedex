68f5b115281e4a5cca08d91773fab6b64bb281bd
hoki9797 <hoki9797@gmail.com>
2025-02-05 14:06:31 +0900

    BrowserRouter 에서 createBrowerRouter로 변경

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -3,13 +3,15 @@ import styled from "styled-components";
 const PokemonsList = styled.div`
     width: 1200px;
     display: grid;
-    grid-template-columns: repeat(6, auto);
-    background-color: #e69224
+    grid-template-columns: repeat(6, 200px);
+    justify-content: center;
+    background-color: #e69224;
 `
 
 const style = {
     width:"150px",
     height:"200px",
+    backgroundColor:"blue"
 }
 
 function PokemonList() {
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "../pages/Home";
 import Dex from "../pages/Dex";
 import Detail from "../pages/Detail";
 
+const router = createBrowserRouter([
+    { path: "/", element: <Home /> },
+    { path: "/dex", element: <Dex /> },
+    { path: "/detail", element: <Detail /> },
+]);
+
 const Router = () => {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="dex" element={<Dex />} />
-                <Route path="detail" element={<Detail />} />
-            </Routes>
-        </BrowserRouter>
+        <RouterProvider router={router} />
     );
 };
 
